0c2bfcf304b417b233c36cd35263347d2f7aab14
MecyDev <macmc@tutanota.com>
2021-06-09 16:55:55 +0200

    refactoring finish. add method for genrate card

--- js/home.js
+++ js/home.js
@@ -1,27 +1,23 @@
-fetch("http://127.0.0.1:5500/js/json/FishEyeData.json")
+const host = window.location.host;
+
+fetch(`http://${host}/js/json/FishEyeData.json`)
   .then((data) => data.json())
   .then((jsonPhotographers) => {
     for (let jsonPhotograph of jsonPhotographers.photographers) {
-      let photograph = new Photograph(jsonPhotograph);
-      document.querySelector("main").innerHTML += `
-      <div class="card">
-        <a class="card__link" href="#">
-          <img class="card__img" src="./img/Photo-id/${
-            photograph.portrait
-          }" alt="" />
-          <h2 class="card__h2">${photograph.name}</h2>
-        </a>
-        <p class="card__from">${photograph.city}, ${photograph.country}</p>
-        <p class="card__tagline"><strong>${photograph.tagline}</strong></p>
-        <p class="card__price">${photograph.price}€/jour</p>
-        <ul class="inline">
-        ${photograph.tags
-          .map(function (e) {
-            return `<li class="tag"><strong>#${e}</strong></li>`;
-          })
-          .join("")}
-        </ul>
-      </div>
-      `;
+      const photograph = new Photograph(jsonPhotograph);
+      document.querySelector("main").innerHTML += photograph.card;
     }
   });
+
+const tags = document.querySelectorAll(".tag");
+let listTags = [];
+
+tags.forEach(
+  (tag) =>
+    tag.addEventListener("click", function () {
+      listTags.push("test");
+    }),
+  false
+);
+
+console.log(listTags);
--- js/photograph.js
+++ js/photograph.js
@@ -4,6 +4,34 @@
 
 class Photograph {
   constructor(photograph) {
-    photograph && Object.assign(this, photograph);
+    this.id = photograph.id;
+    this.name = photograph.name;
+    this.city = photograph.city;
+    this.country = photograph.country;
+    this.tags = photograph.tags;
+    this.tagline = photograph.tagline;
+    this.price = photograph.price;
+    this.portrait = photograph.portrait;
+  }
+
+  get card() {
+    const card = `<div class="card" id=${this.id}>
+        <a class="card__link" href="#">
+          <img class="card__img" src="./img/Photo-id/${this.portrait}" alt="" />
+          <h2 class="card__h2">${this.name}</h2>
+        </a>
+        <p class="card__from">${this.city}, ${this.country}</p>
+        <p class="card__tagline"><strong>${this.tagline}</strong></p>
+        <p class="card__price">${this.price}€/jour</p>
+        <ul class="inline">
+        ${this.tags
+          .map(function (e) {
+            return `<li class="tag"><strong>#${e}</strong></li>`;
+          })
+          .join("")}
+        </ul>
+      </div>`;
+
+    return card;
   }
 }
